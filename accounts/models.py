e284a03573849ccbacf1acc9f3c34e1b6be25776
unknown <pheydari33@yahoo.com>
2026-06-03 16:16:46 +0330

    User model added to the project

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    country = models.CharField(max_length=50, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
