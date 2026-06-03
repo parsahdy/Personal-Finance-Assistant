b52b840b1d96330d9efedb9788e3eea82869beac
unknown <pheydari33@yahoo.com>
2026-06-03 16:24:27 +0330

    Create APIViews for managing accounts and users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer, ProfileSerializer, ChangePasswordSerializer
+
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ProfileView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = ProfileSerializer(request.user)
+        return Response(serializer.data)
+    
+
+class ProfileUpdateView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request):
+        serializer = ProfileSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class ChangePasswordView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            if not user.check_password(serializer.validated_data["old_password"]):
+                return Response({"error": "Old password is incorrect."})
+            
+            user.set_password(serializer.validated_data["new_password"])
+            user.save()
+
+            return Response({"message": "Password changed successfully."})
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
