f3cf14116e9cf3aa2bc4ca6cabe1e21de8175f4d
unknown <pheydari33@yahoo.com>
2026-06-04 19:28:26 +0330

    Expense model created

--- finance/models.py
+++ finance/models.py
@@ -21,8 +21,22 @@ class Income(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="incomes")
     title = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="incomes")
     description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="incomes")
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expenses")
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="expenses")
     date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
