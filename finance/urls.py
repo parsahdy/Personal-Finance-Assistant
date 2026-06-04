a3ce0922bbff6b3fffc18d86ed9ee365cbdd13a9
unknown <pheydari33@yahoo.com>
2026-06-04 19:33:53 +0330

    Setting urls for expense views

--- finance/urls.py
+++ finance/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path("income/detail/<int:pk>/", views.IncomeDetail.as_view(), name="income-detail"),
     path("income/update/<int:pk>/", views.UpdateIncome.as_view(), name="update-income"),
     path("income/delete/<int:pk>/", views.DeleteIncome.as_view(), name="delete-income"),
+
+    path("expense/add/", views.AddExpense.as_view(), name="add-expense"),
+    path("expense/list/", views.ExpenseList.as_view(), name="list-expense"),
+    path("expense/detail/<int:pk>/", views.ExpenseDetail.as_view(), name="expense-detail"),
+    path("expense/update/<int:pk>/", views.UpdateExpense.as_view(), name="update-expense"),
+    path("expense/delete/<int:pk>/", views.DeleteExpense.as_view(), name="delete-expens"),
 ]
