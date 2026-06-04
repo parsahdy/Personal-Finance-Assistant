9cc2811e9e685fe1da30440a4db09729169b6900
unknown <pheydari33@yahoo.com>
2026-06-04 17:08:20 +0330

    Income model created

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class CategoryType(models.TextChoices):
+    INCOME = "income", "income"
+    EXPENSE = "expense", "Expense"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True, db_index=True)
+    category_type = models.CharField(max_length=10, choices=CategoryType.choices)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="incomes")
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name="incomes")
+    description = models.TextField(blank=True)
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
