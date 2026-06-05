a11b0da2f90d4ba67ff6e6afbc19bd7c3a80e503
unknown <pheydari33@yahoo.com>
2026-06-05 11:42:09 +0330

    Created views for adding, updating... and deleting expenses

--- finance/views.py
+++ finance/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from django.shortcuts import get_object_or_404
 
 from .models import Income, Expense
