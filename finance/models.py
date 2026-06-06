e4629a7969009ab75e260d9ff8a5336e0c013101
unknown <pheydari33@yahoo.com>
2026-06-06 15:12:59 +0330

    Budget model created

--- finance/models.py
+++ finance/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 
 
 class CategoryType(models.TextChoices):
-    INCOME = "income", "income"
+    INCOME = "income", "Income"
     EXPENSE = "expense", "Expense"
 
 
@@ -43,3 +43,15 @@ class Expense(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Budget(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="budgets")
+    category = models.ForeignKey(Category, blank=False, null=False, on_delete=models.CASCADE, related_name="budgets")
+    limit_amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False, null=False)
+    date = models.DateField()
+    creted_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category
