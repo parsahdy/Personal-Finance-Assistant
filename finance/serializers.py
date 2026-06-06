0675e1926438a1bcf27a6d0b65d92556c260ad49
unknown <pheydari33@yahoo.com>
2026-06-06 15:14:00 +0330

    Budget API serializer created

--- finance/serializers.py
+++ finance/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from .models import Income, Expense
+from .models import Income, Expense, Budget, Category
 
 
 class IncomeSerializer(serializers.ModelSerializer):
+
+    category = serializers.CharField(
+        required=False, 
+        allow_blank=True, 
+        allow_null=True
+    )
 
     class Meta:
         model = Income
@@ -11,8 +17,15 @@ class IncomeSerializer(serializers.ModelSerializer):
             "title",
             "amount",
             "description",
+            "category",
             "date"
         )
+
+    def create(self, validated_data):
+        category_name = validated_data.pop("category", None)
+        category, _ = Category.objects.get_or_create(name=category_name)
+        validated_data["category"] = category
+        return Income.objects.create(**validated_data)
 
 
 class IncomeDetailSerializer(serializers.ModelSerializer):
@@ -33,14 +46,27 @@ class IncomeDetailSerializer(serializers.ModelSerializer):
 
 class ExpenseSerializer(serializers.ModelSerializer):
 
+    category = serializers.CharField(
+        required=False,
+        allow_blank=True, 
+        allow_null=True
+    )
+
     class Meta:
         model = Expense
         fields = (
             "title",
             "amount",
             "description",
+            "category",
             "date"
         )
+
+    def create(self, validated_data):
+        category_name = validated_data.pop("category", None)
+        category, _ = Category.objects.get_or_create(name=category_name)
+        validated_data["category"] = category
+        return Expense.objects.create(**validated_data)
 
 
 class ExpenseDetailSerializer(serializers.ModelSerializer):
@@ -56,4 +82,38 @@ class ExpenseDetailSerializer(serializers.ModelSerializer):
             "date",
             "created_at",
             "updated_at",
+        )
+
+
+    
+class BudgetSerializer(serializers.ModelSerializer):
+ 
+    category = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True
+    )
+
+    class Meta:
+        model = Budget
+        fields = (
+            "category",
+            "limit_amount",
+            "date"
+        )
+
+    def create(self, validated_data):
+        category_name = validated_data.pop("category", None)
+        category, _ = Category.objects.get_or_create(name=category_name)
+        validated_data["category"] = category
+        return Budget.objects.create(**validated_data)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = (
+            "name",
+            "category_type"
         )
