82662257dd7e63b14b15ebe755261500791d445e
unknown <pheydari33@yahoo.com>
2026-06-08 21:33:25 +0330

    include advisor app urls to main project

--- Finance_project/urls.py
+++ Finance_project/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("accounts.urls")),
     path('finance/', include("finance.urls")),
+    path('advisor/', include("advisor.urls")),
 ]
