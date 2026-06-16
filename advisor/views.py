1a4d9306a271f0c78764a546753a0b22d5cf2429
unknown <pheydari33@yahoo.com>
2026-06-16 22:14:56 +0330

    Recomendation View created

--- advisor/views.py
+++ advisor/views.py
@@ -7,6 +7,7 @@ from django.core.cache import cache
 from advisor.services.chat import FinancialChatServices
 from advisor.ml.predictor import LLMPredictor
 from advisor.serializers import QuestionSerializer
+from advisor.rag.llm_connector import llm_connector
 
 
 class FinancialChatView(APIView):
@@ -57,6 +58,33 @@ class FinancialForecastingView(APIView):
         result = LLMPredictor.forecast(
             question=question,
             user=request.user
+        )
+        return Response({
+            "answer": result
+        })
+    
+
+class  RecomendationView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        serializer = QuestionSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        serializer.save(user=request.user)
+
+        question = serializer.validated_data["question"]
+
+        result = llm_connector(
+            question=question,
+            user=request.user,
         )
         return Response({
             "answer": result
