262bf84dd3191690ae17ad29302eb7a90fb8e4af
unknown <pheydari33@yahoo.com>
2026-06-17 13:36:07 +0330

    The rag manager file modifed

--- advisor/rag/rag_manager.py
+++ advisor/rag/rag_manager.py
@@ -1,42 +1,25 @@
 from pathlib import Path
-
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings 
 from langchain_chroma import Chroma
 
+from .build_vector_db import build_vectorstore
+
 
 RAG_DIR = Path(__file__).resolve().parent 
-DOCUMENT_DIR = RAG_DIR / "documents" 
+PERSIST_DIR = RAG_DIR / "chroma_db"
 
 
-def build_vectorstore():
-    docs = []
 
-    for pdf_file in DOCUMENT_DIR.glob("*.pdf"):
-        loader = PyPDFLoader(str(pdf_file))
-        docs.extend(loader.load())
-
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
-    )
-
-    chunks = splitter.split_documents(docs)
-
-    embeddings = HuggingFaceEmbeddings(
+embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-mpnet-base-v2",
         encode_kwargs={"normalize_embeddings": True},
     )
 
-    return Chroma.from_documents(
-        documents=chunks,
-        embedding=embeddings,
-        persist_directory=str(RAG_DIR / "chroma_db"),
-        collection_name="finance_collection"
-    )
-
-vector_store = build_vectorstore()
+vector_store = Chroma(
+    persist_directory=str(PERSIST_DIR),
+    embedding_function=embeddings,
+    collection_name="finance_collection"
+)
 
 retriever = vector_store.as_retriever(
     search_kwargs={"k": 3}
